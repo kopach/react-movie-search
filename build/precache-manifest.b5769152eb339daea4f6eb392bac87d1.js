bbeb0b4b453cd9370e7eae200209e3a1bfbc1d24
Igor Kopach <kopach@users.noreply.github.com>
2021-04-12 11:15:36 +0200

    Add build files to repo to serve them

---
+++ build/precache-manifest.b5769152eb339daea4f6eb392bac87d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8867dbba1c4e672ef91f63b9902f745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b31c2b6df9e09911b9",
+    "url": "/static/css/2.e7f3280a.chunk.css"
+  },
+  {
+    "revision": "467a7377e9d0cd75b024",
+    "url": "/static/css/main.e3e91b49.chunk.css"
+  },
+  {
+    "revision": "b3b31c2b6df9e09911b9",
+    "url": "/static/js/2.ee25e3c0.chunk.js"
+  },
+  {
+    "revision": "cf90e3ff3b1f121819d35f272431f576",
+    "url": "/static/js/2.ee25e3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467a7377e9d0cd75b024",
+    "url": "/static/js/main.987fab46.chunk.js"
+  },
+  {
+    "revision": "c4dfa24384fd6dea6a0e",
+    "url": "/static/js/runtime-main.5d5a9925.js"
+  }
+]);
